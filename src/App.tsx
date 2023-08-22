9a2aae0ba17013cf4a807639b46bddedf83c8366
oneraner <oneraner@gmail.com>
2023-08-22 19:43:27 +0800

    feat: add test file

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,35 @@ export interface GetListResponse {
 
 function App() {
   const wrapRef = useRef<HTMLDivElement>(null);
-  const { data } = useSWR("video", () =>
+  const params = new URLSearchParams(window.location.search);
+  const isMock = params.get("mock") === "true";
+
+  const { data } = useSWR(isMock ? null : "video", () =>
     axios.get("http://localhost:8088/for_you_list").then(res => res)
   );
+
+  const shortsData = isMock
+    ? [
+        {
+          title: "test0",
+          play_url: "https://livedoc.cgtn.com/500d/prog_index.m3u8",
+        },
+        {
+          title: "test1",
+          play_url: "https://livedoc.cgtn.com/500d/prog_index.m3u8",
+        },
+        {
+          title: "test2",
+          play_url: "https://livedoc.cgtn.com/500d/prog_index.m3u8",
+        },
+      ]
+    : data?.data?.items ?? [];
 
   return (
     <div ref={wrapRef} className="container">
       <Header />
       <ShortsPlayer
-        shortsData={data?.data?.items ?? []}
+        shortsData={shortsData}
         screenHeight={wrapRef?.current?.clientHeight ?? 0}
       />
       <Footer />
